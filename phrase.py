db7ab62891010be1c30b12dfc3ede217611f64cb
AsgothXentio <152388913+AsgothXentio@users.noreply.github.com>
2024-01-16 12:32:10 +0100

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -4,6 +4,7 @@ class Phrase:
         self.phrase = active_phrase
         self.game_instance = game_instance
 
+    
     def display(self):
         for letter in self.phrase:
             if letter.lower() in self.game_instance.guesses:

7d93d2a085342ae7f5ca7c19db67d3249c43cfe1
AsgothXentio <152388913+AsgothXentio@users.noreply.github.com>
2024-01-16 10:05:16 +0100

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -1,1 +1,19 @@
 
+class Phrase:
+    def __init__(self, active_phrase, game_instance):
+        self.phrase = active_phrase
+        self.game_instance = game_instance
+
+    def display(self):
+        for letter in self.phrase:
+            if letter.lower() in self.game_instance.guesses:
+                print(f"{letter}", end=" ")
+            elif letter == " ":
+                print(" ", end=" ")
+            else:
+                print("_", end=" ")
+        print()  # Add a newline after displaying the phrase
+
+
+
+
